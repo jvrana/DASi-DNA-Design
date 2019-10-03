a821760d456d528655a708c59ee1fc7c313b6325
Justin Vrana <justin.vrana@gmail.com>
2019-10-03 15:13:31 -0700

    update docs

--- tests/test_design_sequences.py
+++ tests/test_design_sequences.py
@@ -10,6 +10,34 @@ bases += bases.lower()
 gfp = "ATGGTCTCTAAGGGTGAAGAATTGTTCACCGGTGTCGTCCCAATCTTGGTCGAATTGGACGGGGACGTCAACGGTCACAAGTTCTCTGTCTCTGGTGAAGGTGAAGGTGACGCTACCTACGGTAAGTTGACCTTGAAGTTCATCTGTACCACCGGTAAGTTGCCAGTCCCATGGCCAACCTTGGTCACCACCTTCGGTTACGGTGTCCAATGTTTCGCTAGATACCCAGACCACATGAAGCAACACGACTTCTTCAAGTCTGCTATGCCAGAAGGTTACGTCCAAGAAAGAACCATCTTCTTCAAGGACGACGGTAACTACAAGACCAGAGCTGAAGTCAAGTTCGAAGGTGACACCTTGGTCAACAGAATCGAATTGAAGGGTATCGACTTCAAGGAAGACGGTAACATCTTGGGTCACAAGTTGGAATACAACTACAACTCTCACAACGTCTACATCATGGCTGACAAGCAAAAGAACGGTATCAAGGTCAACTTCAAGATCAGACACAACATCGAAGACGGTTCTGTCCAATTGGCTGACCACTACCAACAAAACACCCCAATCGGTGACGGTCCAGTCTTGTTGCCAGACAACCACTACTTGTCTACCCAATCTGCTTTGTCTAAGGACCCAAACGAAAAGAGAGACCACATGGTCTTGTTGGAATTCGTCACCGCTGCTGGTATCACCCACGGTATGGACGAATTGTACAAGTAA"
 
 
+def test_region_invert():
+    """Check Region.invert.
+
+    This is essential to the primer design algorithm.
+    """
+    r = Region(10, 20, 50, cyclic=True)
+    r1, r2 = r.invert()
+    assert r2 is None
+    assert list(r1) == list(range(20, 50)) + list(range(10))
+
+
 def test_primer_design():
-    pairs, explain = design_primers(gfp, Region(50, 300, len(gfp)), None, None)
+    region = Region(100, 300, len(gfp), cyclic=True)
+    pairs, explain = design_primers(gfp, region, None, None)
     print(json.dumps(pairs, indent=1))
+
+    for pair in pairs.values():
+        assert pair["LEFT"]["location"][0] == 100
+        assert pair["RIGHT"]["location"][0] == 299
+
+
+def test_primer_design2():
+    i = len(gfp) - 50
+    j = 100
+    region = Region(i, j, len(gfp), cyclic=True)
+    pairs, explain = design_primers(gfp, region, None, None)
+    # print(json.dumps(pairs, indent=1))
+    for pair in pairs.values():
+        print(pair["LEFT"]["location"])
+        assert pair["LEFT"]["location"][0] == i
+        assert pair["RIGHT"]["location"][0] == j - 1
